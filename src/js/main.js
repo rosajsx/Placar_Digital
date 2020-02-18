4e39d8b936c0c4cb244b5b7470790275a2d6630c
João Pedro Santos <betajpsg@gmail.com>
2020-02-18 20:43:37 -0300

    updating view

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+function trocarImgHome(){
+    let srcAtual = document.getElementById('homeImg').src = "../time1.jpg";
+    // alteração de propriedade a partir do ID
+}
+
+function trocarImgVisitor(){
+    let srcAtual = document.getElementById('visitorImg').src = "../time2.jpg";
+    // alteração de propriedade a partir do ID
+}
+
+
