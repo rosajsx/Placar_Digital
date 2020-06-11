d1712b69f84646b3118dd2ae33aac40458b326be
Lucas Rosa <55213609+Bluniz@users.noreply.github.com>
2020-06-10 22:27:48 -0300

    Consolidação da Pag de cronometro
    -Deletado pags desnecessárias
    -Unificação de pags com definição de tempo

--- src/js/script_junior.js
+++ src/js/script_junior.js
@@ -1,12 +1,16 @@
-<<<<<<< HEAD
 var configMinuto;
 var configSegundo;
 var mostrarValor;
 var evento = null;
 var contador = null;
-var minuto;
-var segundo;
 var p1=0, p2=0;
+const value = document.querySelector("#teste");
+const btnELAumentar = document.querySelector("#teste1");
+const btnElDiminuir = document.querySelector("#teste2")
+
+
+
+
 
 
 function IniciarCronometro(valor){
@@ -23,8 +27,19 @@ function IniciarCronometro(valor){
 			document.getElementById('btnIniciar').disabled  = true;
 			document.getElementById('btnResetar').disabled  = false;
 			document.getElementById('btnPausar').disabled  = false;
-			this.minuto = 30;
-			this.segundo = 00;
+
+			let result = value.value.split(":");
+			let result_min = parseFloat(result[0]);
+			let result_seg = parseFloat(result[1]);
+			
+
+			this.minuto = result_min;
+			this.segundo = result_seg;
+
+			btnELAumentar.disabled = true;
+			btnElDiminuir.disabled = true;
+      
+			
 		}else{
 			if(this.segundo == 0 && this.minuto != 0){
 				this.segundo = 59;
@@ -41,6 +56,7 @@ function IniciarCronometro(valor){
 				this.mostrarValor.value = "00:00";
 				
 				clearTimeout(this.contador);
+
 				return;
 			}	
 			
@@ -80,8 +96,10 @@ function ResetarCronometro(){
 	document.getElementById('btnIniciar').disabled  = false;
 	document.getElementById('btnResetar').disabled  = true;
 	document.getElementById('btnPausar').disabled  = true;
+	btnELAumentar.disabled = false;
+	btnElDiminuir.disabled = false;
 	document.getElementById('btnPausar').value = "PAUSAR";
-	this.mostrarValor.value = "30:00";
+	this.mostrarValor.value = "00:00";
 
  document.getElementById('mostrarValor').classList.remove('mostrarValor2');
 	document.getElementById('mostrarValor').classList.add('mostrarValor');
@@ -100,106 +118,33 @@ function golTime2(){
 	p2 +=1;
 	document.getElementById('gol_2').innerHTML = p2;
 }
-=======
-var configMinuto;
-var configSegundo;
-var mostrarValor;
-var evento = null;
-var contador = null;
-var minuto;
-var segundo;
-var p1=0, p2=0;
 
 
-function IniciarCronometro(valor){
-	this.evento = valor;
-	this.configMinuto = document.getElementById('min').value;
-	this.configSegundo = document.getElementById('seg').value;
-	this.mostrarValor = document.getElementById('mostrarValor');
-	
-	
-	if (evento=="start"){
-		if(!document.getElementById('min').readOnly){
-			document.getElementById('min').readOnly = true;
-			document.getElementById('seg').readOnly = true;
-			document.getElementById('btnIniciar').disabled  = true;
-			document.getElementById('btnResetar').disabled  = false;
-			document.getElementById('btnPausar').disabled  = false;
-			this.minuto = 30;
-			this.segundo = 00;
-		}else{
-			if(this.segundo == 0 && this.minuto != 0){
-				this.segundo = 59;
-				this.minuto--;
-			}else{
-				this.segundo--;
-			}
-			if(this.minuto == 0 && this.segundo == 0){
-				document.getElementById('min').readOnly = false;
-				document.getElementById('seg').readOnly = false;
-				document.getElementById('btnIniciar').disabled  = false;
-				document.getElementById('btnResetar').disabled  = true;
-				document.getElementById('btnPausar').disabled  = true;
-				this.mostrarValor.value = "00:00";
-				
-				clearTimeout(this.contador);
-				return;
-			}	
-			
-			novoMinuto = null;
-			novoSegundo = null;
-			if(this.minuto <= 9){
-				novoMinuto = "0" + this.minuto;
-			}else{
-				novoMinuto = this.minuto;
-			}
-			if(this.segundo <= 9){
-				novoSegundo = "0" + this.segundo;
-			}else{
-				novoSegundo = this.segundo;
-			}
-			this.mostrarValor.value = novoMinuto + ":" + novoSegundo;
-		}
+function increase(){
+
+	 //retorna valores separados pelo :
+	 let valor_separado = value.value.split(":");
+
+	 let min = parseFloat(valor_separado[0]);
+	 let seg = valor_separado[1];
+
+	 min += 05; 
+
+	 value.value = min + ":" + seg;
+}
+
+function decrease(){
+
+	let valor_separado = value.value.split(":");
+	let min = parseFloat(valor_separado[0]);
+	let seg = valor_separado[1];
+
+	if(min == 0){
+		return
 	}
-	clearTimeout(this.contador);
-	this.contador = setTimeout('IniciarCronometro(evento)', 1000);
+
+	min -= 05;
+
+	value.value = min + ":" + seg;
+
 }
-
-
-function PausarCronometro(){
-	if(document.getElementById('btnPausar').value=="PAUSAR"){
-		document.getElementById('btnPausar').value = "VOLTAR";
-		this.evento = "pause";
-	}else{
-		document.getElementById('btnPausar').value = "PAUSAR";
-		this.evento = "start";
-	}
-}
-
-function ResetarCronometro(){
-	document.getElementById('min').readOnly = false;
-	document.getElementById('seg').readOnly = false;
-	document.getElementById('btnIniciar').disabled  = false;
-	document.getElementById('btnResetar').disabled  = true;
-	document.getElementById('btnPausar').disabled  = true;
-	document.getElementById('btnPausar').value = "PAUSAR";
-	this.mostrarValor.value = "30:00";
-
- document.getElementById('mostrarValor').classList.remove('mostrarValor2');
-	document.getElementById('mostrarValor').classList.add('mostrarValor');
-	document.getElementById('exibe').classList.remove('Classexibe2');
-	document.getElementById('exibe').classList.add('Classexibe');
-
-	clearTimeout(this.contador);
-}
-
-function golTime1(){
-	p1 +=1;
-	document.getElementById('gol_1').innerHTML = p1;
-}
-
-function golTime2(){
-	p2 +=1;
-	document.getElementById('gol_2').innerHTML = p2;
-}
->>>>>>> 3b9227a8f8a9cfbc52ee9527f7026225a911a383
--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { app, BrowserWindow } = require('electron')
 // Mantém a referência global do objeto da janela.
 // se você não fizer isso,
@@ -54,61 +53,4 @@ app.on('activate', () => {
 })
 
 // Neste arquivo, você pode incluir o resto do seu aplicativo especifico do processo
-=======
-const { app, BrowserWindow } = require('electron')
-// Mantém a referência global do objeto da janela.
-// se você não fizer isso,
-// a janela será fechada automaticamente
-// quando o objeto JavaScript for coletado como lixo.
-let win
-
-function createWindow () {
-  // Criar uma janela de navegação.
-  win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
-
-  // and load the index.html of the app.
-  win.loadFile('./src/index.html')
-
-  // Open the DevTools.
-  win.webContents.openDevTools()
-
-  // Emitido quando a janela é fechada.
-  win.on('closed', () => {
-    // Elimina a referência do objeto da janela, geralmente você iria armazenar as janelas
-    // em um array, se seu app suporta várias janelas, este é o momento
-    // quando você deve excluir o elemento correspondente.
-    win = null
-  })
-}
-
-// Este método será chamado quando o Electron tiver finalizado
-// a inicialização e está pronto para criar a janela browser.
-// Algumas APIs podem ser usadas somente depois que este evento ocorre.
-app.on('ready', createWindow)
-
-// Finaliza quando todas as janelas estiverem fechadas.
-app.on('window-all-closed', () => {
-  // No macOS é comum para aplicativos e sua barra de menu 
-  // permaneçam ativo até que o usuário explicitamente encerre com Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow()
-  }
-})
-
-// Neste arquivo, você pode incluir o resto do seu aplicativo especifico do processo
->>>>>>> 3b9227a8f8a9cfbc52ee9527f7026225a911a383
 // principal. Você também pode colocar eles em arquivos separados e requeridos-as aqui.
